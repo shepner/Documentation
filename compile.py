e6f21eb9ed1a93659d92be2c1f1b8b14d6b0c58f
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2019-10-29 21:09:37 -0500

    Update compile.py

--- compile.py
+++ compile.py
@@ -1,7 +1,15 @@
 #!`which python3`
 
+"""
+# experimenting with the idea of using a mix of YAML and MD for the psodocode/commentary/discussion/etc 
+# outside of the code's comments to see how it works.  Or perhaps something similar to
+# the Jupyter Notebook (.IPYNB) file format may also be an option?  Need to explore
+"""
+
+"""
 #read file and parse YAML data
 #pip install pyyaml
+"""
 import yaml
 
 with open(r'E:\data\categories.yaml') as file:
@@ -9,10 +17,10 @@ with open(r'E:\data\categories.yaml') as file:
     for item, doc in documents.items():
         print(item, ":", doc)
 
-###########
-
+"""
 #convert [MD](https://github.com/readthedocs/commonmark.py) to html
 #pip install commonmark
+"""
 import commonmark
 
 parser = commonmark.Parser()
@@ -21,11 +29,9 @@ renderer = commonmark.HtmlRenderer()
 html = renderer.render(ast)
 print(html)
 
+
+"""
 #output file to destination site
 
-###########
-
 #call [pandoc](https://pandoc.org/) to convert
-
-
-
+"""
