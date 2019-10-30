279143339fd8b9929472601688675cd7b0dd6ead
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2019-10-30 08:31:17 -0500

    Create summarize.py

---
+++ summarize.py
@@ -0,0 +1,31 @@
+#!`which python3`
+"""
+# summarize.py
+
+## Purpose
+
+The purpose of this application is to scan through all of the documents within the repository and generate a single summary file which can be used to access them
+"""
+
+"""
+## Structure
+
+``` psudocode
+read contents of repo
+  if *.yaml file
+    parse YAML
+    generate tabular output in html:
+      layout: "[title](URL)", "shortDescription", "most recent revision date", "most recent revision status"
+      convert MD to html
+save file to summary.html
+```
+
+## References
+* [PyYAML](https://pyyaml.org/)
+* [CommonMark/MarkDown](https://github.com/readthedocs/commonmark.py)
+
+"""
+
+# code goes here
+
+
